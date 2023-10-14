529c4e945ade89253f73444ff57ddb6dc00b3add
Samson Dada <57550434+Samson-Dada@users.noreply.github.com>
2023-10-14 22:56:30 +0100

    Inital Commit

---
+++ Controllers/ResponseController.cs
@@ -0,0 +1,24 @@
+﻿using Endpoint.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Endpoint.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ResponseController : ControllerBase
+    {
+      
+        [HttpGet("message")]
+        public ActionResult<Response> GetMessage()
+        {
+            var Response = new Response
+            {
+                Message = "Automate all the things Dolapo !",
+                Timestamp = DateTimeOffset.Now.ToUnixTimeSeconds()
+            };
+
+            return  Ok(Response);
+        }
+    }
+}
---
+++ Model/Response.cs
@@ -0,0 +1,9 @@
+﻿namespace Endpoint.Model
+{
+    public class Response
+    {
+       
+            public string? Message { get; set; }
+            public long Timestamp { get; set; }
+    }
+}
